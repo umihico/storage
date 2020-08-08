bbe89bdaecd34d0bcc0006ed590500c1e6ea5737
umihico <umihico@users.noreply.github.com>
2020-08-08 11:31:45 +0900

    first commit

--- handler.py
+++ handler.py
@@ -1,24 +1,33 @@
 import json
+import boto3
+import os
+import time
+import random
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def invalidate(event, context):
+    print(event)
+    Items = ["/" + record['s3']['object']['key']
+             for record in event['Records']]
+    client = boto3.client("cloudfront")
+    response = client.create_invalidation(
+        DistributionId=os.getenv("CLOUDFRONT_DISTRIBUTION_ID"),
+        InvalidationBatch={
+            'Paths': {
+                'Quantity': len(Items),
+                'Items': Items,
+            },
 
+            # borrowed from aws-cli/awscli/customizations/cloudfront.py
+            'CallerReference': '%s-%s-%s' % ('cli', int(time.time()), random.randint(1, 1000000))
+        }
+    )
+    print(response)
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps({
+            'event': event,
+            'response': str(response)
+        })
     }
-
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
